dd708fb13c23f704b4c56f3e2aa8a0bcc436122d
Johnson Howard <jhoward@adaptavist.com>
2023-06-14 11:24:28 +0100

    thirty9

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -1,6 +1,6 @@
 module "sqs_message_processor" {
   source                 = "Adaptavist/aws-lambda/module"
-  version                = "1.33.0"
+  version                = "1.34.0"
   name                   = var.function_name
   namespace              = var.namespace
   stage                  = var.stage
